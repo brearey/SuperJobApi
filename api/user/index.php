bac599a341051ad3b3a88e4b38633a2d654fe7f2
Brearey <brearey4@gmail.com>
2023-03-05 20:44:53 +0900

    Feature GET one user with token

---
+++ api/user/index.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once ('../include/functions.php');
+
+require_once ('../Message.php');
+require_once ('../repo/Repository.php');
+
+use api\Message;
+use repo\Repository;
+
+header("Content-type: application/json; charset=utf-8");
+
+$repo = new Repository();
+
+if (checkAppKey()) {
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $result = $repo->get_user_by_token($_SERVER['HTTP_TOKEN']);
+        if (!$result->status) {
+            die(json_encode(new Message(false, $result->text)));
+        } else {
+            echo (json_encode(new Message(true, $result->text)));
+        }
+
+        //echo json_encode($repo->get_users());
+    } else {
+        http_response_code(400);
+        die(json_encode(new Message(false, "Требуется GET запрос")));
+    }
+}
