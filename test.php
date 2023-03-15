4f08c756285f70547c9d28d178b05022931acf8a
brearey <brearey4@gmail.com>
2023-03-15 12:17:27 +0900

    Learn PDO

--- test.php
+++ test.php
@@ -1,17 +1,57 @@
 <?php
 
-db_connect('test');
-function db_connect($db_name) {
-    $path = '';
+header('Content-type: application/json');
 
-    $db = new SQLite3($path.$db_name . '.db');
-    if (!$db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='{message}';")) {
-        $db = new SQLite3($path.$db_name . '.db');
-        $sql="CREATE TABLE message (
-            message_text TEXT NOT NULL,
-            sender_name TEXT NOT NULL
-        )";
-        $db->query($sql);
+$db = db_connect('test');
+selectMessageByReceiver($db, 'artur_token');
+
+function db_connect($db_name) {
+    if (!file_exists($db_name . ".db")) {
+        $db = new PDO("sqlite:" . $db_name . ".db");
+        createMessageTable($db);
+        createWorkerTable($db);
+        return $db;
     }
+    $db = new PDO("sqlite:" . $db_name . ".db");
     return $db;
+}
+
+function createMessageTable(PDO $db) {
+    $sql='CREATE TABLE message (
+            sender_name TEXT NOT NULL,
+            message_text TEXT NOT NULL,
+            sender_token TEXT NOT NULL,
+            receiver_token TEXT NOT NULL
+        )';
+
+    $db->exec($sql);
+}
+
+function insertMessage(PDO $db) {
+    $sql = "INSERT INTO message (sender_name, message_text, sender_token, receiver_token) VALUES (:sender_name, :message_text, :sender_token, :receiver_token)";
+    $stmt = $db->prepare($sql);
+    $stmt->bindValue(':sender_name', 'jeka');
+    $stmt->bindValue(':message_text', 'message at ' . time());
+    $stmt->bindValue(':sender_token', 'jeka_token');
+    $stmt->bindValue(':receiver_token', 'artur_token');
+    $stmt->execute();
+}
+
+function selectMessageByReceiver(PDO $db, $receiver_token) {
+    $sql = "SELECT * FROM message WHERE receiver_token = '".$receiver_token."'";
+    $statement = $db->prepare($sql);
+    $statement->execute();
+    $result = $statement->fetchAll();
+    echo ( json_encode($result) );
+}
+
+function createWorkerTable(PDO $db) {
+    $sql='CREATE TABLE worker (
+            token TEXT NOT NULL,
+            name TEXT NOT NULL,
+            age INTEGER NOT NULL,
+            town TEXT NOT NULL
+        )';
+
+    $db->exec($sql);
 }
