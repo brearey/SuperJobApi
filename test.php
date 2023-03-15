761d0379c4cbc0e5231db2b4a83a7cc0ded43a83
brearey <brearey4@gmail.com>
2023-03-15 14:59:28 +0900

    prepare one repository

--- test.php
+++ test.php
@@ -1,9 +1,11 @@
 <?php
 
+require_once ('api/inc/Message.php');
+use inc\Message;
+
 header('Content-type: application/json');
 
 $db = db_connect('test');
-selectMessageByReceiver($db, 'artur_token');
 
 function db_connect($db_name) {
     if (!file_exists($db_name . ".db")) {
@@ -27,22 +29,26 @@ function createMessageTable(PDO $db) {
     $db->exec($sql);
 }
 
-function insertMessage(PDO $db) {
+function addMessage(PDO $db, Message $message) {
     $sql = "INSERT INTO message (sender_name, message_text, sender_token, receiver_token) VALUES (:sender_name, :message_text, :sender_token, :receiver_token)";
     $stmt = $db->prepare($sql);
-    $stmt->bindValue(':sender_name', 'jeka');
-    $stmt->bindValue(':message_text', 'message at ' . time());
-    $stmt->bindValue(':sender_token', 'jeka_token');
-    $stmt->bindValue(':receiver_token', 'artur_token');
+    $stmt->bindValue(':sender_name', $message->sender_name);
+    $stmt->bindValue(':message_text', $message->message_text);
+    $stmt->bindValue(':sender_token', $message->sender_token);
+    $stmt->bindValue(':receiver_token', $message->receiver_token);
     $stmt->execute();
 }
 
-function selectMessageByReceiver(PDO $db, $receiver_token) {
+function getMessagesByReceiver(PDO $db, $receiver_token): array {
     $sql = "SELECT * FROM message WHERE receiver_token = '".$receiver_token."'";
     $statement = $db->prepare($sql);
     $statement->execute();
-    $result = $statement->fetchAll();
-    echo ( json_encode($result) );
+
+    $messages = [];
+    while ($row = $statement->fetch(\PDO::FETCH_ASSOC)) {
+        $messages[] = new Message($row['sender_name'],$row['message_text'],$row['sender_token'],$row['receiver_token']);
+    }
+    return $messages;
 }
 
 function createWorkerTable(PDO $db) {
