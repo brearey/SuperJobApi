ada949d3a838ad4116eec197cef6ef1ca8c81d6b
Brearey <brearey4@gmail.com>
2023-03-16 11:29:39 +0900

    Отличия autoload у меня и на хостинге

--- api/autoload.php
+++ api/autoload.php
@@ -2,6 +2,9 @@
 
 spl_autoload_register(
     function (string $class): void {
+
+        //$class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+
         $path = $class . '.php';
         if (is_readable($path)) {
             require_once $path;
